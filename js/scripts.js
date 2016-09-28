0a85e82c454677e62dfb31f8ce30c19f1f3d2026
sam campione <samcampione@sams-MacBook-Pro.local>
2016-09-27 20:14:56 -0500

    create book objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,37 @@
 // Put all of your jQuery and JavaScript in this document.
 
-var book = {
-    title:,
-    author:,
-    isbn:,
-    price:,
-    imageFrontAddress:,
-    imageBackAddress:
+var itemCount;
+
+var book1 = {
+    "id":1,
+    "name": "JavaScript - The Good Parts",
+    "author":"Dough Crawford",
+    "price":12.00,
+    "picture_url":"https://images-na.ssl-images-amazon.com/images/I/5166ztxOm9L._SX381_BO1,204,203,200_.jpg",
+    "selling_points":["Best javascript book of 2016","Great example code"]
 }
 
-var loadBook = function() {
-    getBookImage();
-    getBookAuthor();
+var book2 = {
+    "id":2,
+    "name":"Exploring ReactJS",
+    "author":"Edward Smith",
+    "price":19.99,
+    "picture_url":"http://reactkungfu.com/assets/images/rbe-cover.png",
+    "selling_points":["Great book for beginners"]
 }
-
-var getBookImage = function(bookIsbn) {
-    book.imageFrontAddress ="<img src="https://images-na.ssl-images-amazon.com/images/I/5166ztxOm9L._SX381_BO1,204,203,200_.jpg"
+var album1 = {
+    "id":3,
+    "name":"Nevermind",
+    "author":"Nirvana",
+    "price":15.00,
+    "picture_url":"https://upload.wikimedia.org/wikipedia/en/b/b7/NirvanaNevermindalbumcover.jpg",
+    "selling_points":["point 1","point 2"]
 }
-
-var getBookAuthor = function(bookIsbn) {
-    book.author="Douglas Crockford"
-}
-
-var getBookPrice = function(bookIsbn) {
-    book.price=1500
+var album2 = {
+    "id":4,
+    "name":"A Moon-Shaped Pool",
+    "author":"Radiohead",
+    "price":12.95,
+    "picture_url":"http://cdn.pitchfork.com/albums/23244/e3c43867.jpg",
+    "selling_points":["point 1", "point 2", "point 3"]
 }
