b857572deb09514a328a769e60fe07baa1a84fc5
sam campione <samcampione@sams-MacBook-Pro.local>
2016-09-27 19:37:38 -0500

    Started HTML for book content

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,27 @@
 // Put all of your jQuery and JavaScript in this document.
+
+var book = {
+    title:,
+    author:,
+    isbn:,
+    price:,
+    imageFrontAddress:,
+    imageBackAddress:
+}
+
+var loadBook = function() {
+    getBookImage();
+    getBookAuthor();
+}
+
+var getBookImage = function(bookIsbn) {
+    book.imageFrontAddress ="<img src="https://images-na.ssl-images-amazon.com/images/I/5166ztxOm9L._SX381_BO1,204,203,200_.jpg"
+}
+
+var getBookAuthor = function(bookIsbn) {
+    book.author="Douglas Crockford"
+}
+
+var getBookPrice = function(bookIsbn) {
+    book.price=1500
+}
