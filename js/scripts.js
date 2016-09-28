59e6bf54f9d153568ad9b0178a2c516f9894b287
Alex Pearson <apearson@five-startech.com>
2016-09-27 20:49:09 -0500

    Add addToPage function

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@ var book1 = {
     "price":12.00,
     "picture_url":"https://images-na.ssl-images-amazon.com/images/I/5166ztxOm9L._SX381_BO1,204,203,200_.jpg",
     "selling_points":["Best javascript book of 2016","Great example code"]
-}
+};
 
 var book2 = {
     "id":2,
@@ -18,7 +18,8 @@ var book2 = {
     "price":19.99,
     "picture_url":"http://reactkungfu.com/assets/images/rbe-cover.png",
     "selling_points":["Great book for beginners"]
-}
+};
+
 var album1 = {
     "id":3,
     "name":"Nevermind",
@@ -26,7 +27,8 @@ var album1 = {
     "price":15.00,
     "picture_url":"https://upload.wikimedia.org/wikipedia/en/b/b7/NirvanaNevermindalbumcover.jpg",
     "selling_points":["point 1","point 2"]
-}
+};
+
 var album2 = {
     "id":4,
     "name":"A Moon-Shaped Pool",
@@ -34,4 +36,22 @@ var album2 = {
     "price":12.95,
     "picture_url":"http://cdn.pitchfork.com/albums/23244/e3c43867.jpg",
     "selling_points":["point 1", "point 2", "point 3"]
-}
+};
+
+var addToPage = function( productObject, targetId ){
+    var $target = $( "#" + targetId );
+    var $sellingPoints = $target.find( ".selling-points" );
+    var i = 0;
+
+    $target.find( ".name" ).text( productObject.name );
+    $target.find( ".author" ).text( productObject.author );
+    $target.find( ".picture_url").html( "<img src='" + productObject.picture_url + "'>" );
+    $target.find( ".price" ).text( productObject.price );
+
+    while( i < productObject.selling_points.length ){
+        $sellingPoints.append( "<li>" + productObject.selling_points[i] + "</li>");
+        i++;
+    }
+};
+
+addToPage( book1, "book1" );
