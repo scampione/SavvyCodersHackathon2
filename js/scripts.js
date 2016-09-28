1fb744ee13c79da51cc2d77705e0a73904399ed6
sam campione <samcampione@sams-MacBook-Pro.local>
2016-09-27 21:39:23 -0500

    More styling

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,8 @@ var book1 = {
     "name": "JavaScript - The Good Parts",
     "author":"Dough Crawford",
     "price":12.00,
-    "picture_url":"https://images-na.ssl-images-amazon.com/images/I/5166ztxOm9L._SX381_BO1,204,203,200_.jpg",
-    "selling_points":["Best javascript book of 2016","Great example code"]
+    "pictureUrl":"https://images-na.ssl-images-amazon.com/images/I/5166ztxOm9L._SX381_BO1,204,203,200_.jpg",
+    "sellingPoints":["Best javascript book of 2016","Great example code"]
 };
 
 var book2 = {
@@ -17,8 +17,8 @@ var book2 = {
     "name":"Exploring ReactJS",
     "author":"Edward Smith",
     "price":19.99,
-    "picture_url":"http://reactkungfu.com/assets/images/rbe-cover.png",
-    "selling_points":["Great book for beginners"]
+    "pictureUrl":"http://reactkungfu.com/assets/images/rbe-cover.png",
+    "sellingPoints":["Great book for beginners"]
 };
 
 var album1 = {
@@ -26,8 +26,8 @@ var album1 = {
     "name":"Nevermind",
     "author":"Nirvana",
     "price":15.00,
-    "picture_url":"https://upload.wikimedia.org/wikipedia/en/b/b7/NirvanaNevermindalbumcover.jpg",
-    "selling_points":["point 1","point 2"]
+    "pictureUrl":"https://upload.wikimedia.org/wikipedia/en/b/b7/NirvanaNevermindalbumcover.jpg",
+    "sellingPoints":["point 1","point 2"]
 };
 
 var album2 = {
@@ -35,8 +35,8 @@ var album2 = {
     "name":"A Moon-Shaped Pool",
     "author":"Radiohead",
     "price":12.95,
-    "picture_url":"http://cdn.pitchfork.com/albums/23244/e3c43867.jpg",
-    "selling_points":["point 1", "point 2", "point 3"]
+    "pictureUrl":"http://cdn.pitchfork.com/albums/23244/e3c43867.jpg",
+    "sellingPoints":["point 1", "point 2", "point 3"]
 };
 
 var addToPage = function( productObject, targetId ){
@@ -46,11 +46,11 @@ var addToPage = function( productObject, targetId ){
 
     $target.find( ".name" ).text( productObject.name );
     $target.find( ".author" ).text( productObject.author );
-    $target.find( ".picture_url").html( "<img src='" + productObject.picture_url + "'>" );
+    $target.find( ".picture-url").html( "<img src='" + productObject.pictureUrl + "'>" );
     $target.find( ".price" ).text( productObject.price );
 
-    while( i < productObject.selling_points.length ){
-        $sellingPoints.append( "<li>" + productObject.selling_points[i] + "</li>");
+    while( i < productObject.sellingPoints.length ){
+        $sellingPoints.append( "<li>" + productObject.sellingPoints[i] + "</li>");
         i++;
     }
 };
