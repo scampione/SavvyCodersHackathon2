6b61e9890a372a2303e8b1c9f565ce7b92e3e4d7
Alex Pearson <apearson@five-startech.com>
2016-09-27 21:38:48 -0500

    Add fancy pancy scripts

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ var numberOfPoints;
 
 var book1 = {
     "id":1,
+    "type": "book",
     "name": "JavaScript - The Good Parts",
     "author":"Dough Crawford",
     "price":12.00,
@@ -14,6 +15,7 @@ var book1 = {
 
 var book2 = {
     "id":2,
+    "type": "book",
     "name":"Exploring ReactJS",
     "author":"Edward Smith",
     "price":19.99,
@@ -23,6 +25,7 @@ var book2 = {
 
 var album1 = {
     "id":3,
+    "type": "album",
     "name":"Nevermind",
     "author":"Nirvana",
     "price":15.00,
@@ -32,6 +35,7 @@ var album1 = {
 
 var album2 = {
     "id":4,
+    "type": "album",
     "name":"A Moon-Shaped Pool",
     "author":"Radiohead",
     "price":12.95,
@@ -39,15 +43,19 @@ var album2 = {
     "selling_points":["point 1", "point 2", "point 3"]
 };
 
-var addToPage = function( productObject, targetId ){
-    var $target = $( "#" + targetId );
-    var $sellingPoints = $target.find( ".selling-points" );
+var addToPage = function( productObject ){
+    $( "#content-area" ).append( "<div id='" + productObject.type + productObject.id + "'></div>" );
+    var $target = $( "#" + productObject.type + productObject.id );
+    var $sellingPoints;
     var i = 0;
 
-    $target.find( ".name" ).text( productObject.name );
-    $target.find( ".author" ).text( productObject.author );
-    $target.find( ".picture_url").html( "<img src='" + productObject.picture_url + "'>" );
-    $target.find( ".price" ).text( productObject.price );
+    $target.append( "<div class='name'>" + productObject.name + "</div>" );
+    $target.append( "<div class='author'>" + productObject.author + "</div>" );
+    $target.append( $( "<div class='picture_url'></div>").html( "<img src='" + productObject.picture_url + "'>" ) );
+    $target.append( "<div class='price'>" + productObject.price + "</div>" );
+    $target.append( "<div class='selling_points'></div>" );
+
+    $sellingPoints = $target.find( ".selling_points" );
 
     while( i < productObject.selling_points.length ){
         $sellingPoints.append( "<li>" + productObject.selling_points[i] + "</li>");
@@ -55,7 +63,28 @@ var addToPage = function( productObject, targetId ){
     }
 };
 
-addToPage( book1, "book1" );
-addToPage( book2, "book2" );
-addToPage( album1, "album1" );
-addToPage( album2, "album2" );
+addToPage( book1 );
+addToPage( book2 );
+addToPage( album1 );
+addToPage( album2 );
+
+var count = 4;
+
+$( "form" ).on( "submit", function( event ){
+    event.preventDefault();
+
+    var data = $( this ).serializeArray();
+    var formObject = {};
+
+    formObject.id = ++count;
+    formObject.selling_points = [];
+    data.forEach( function( field ){
+        if( field.name === "selling_points" ){
+            formObject.selling_points.push( field.value );
+        } else{
+            formObject[ field.name ] = field.value;
+        }
+    } );
+
+    addToPage( formObject );
+});
